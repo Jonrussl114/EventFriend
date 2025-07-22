fb38b4231bab66c836654ec6b4a12de1c3e8ae19
Jonrussl114 <jonrussl114@gmail.com>
2025-07-22 01:44:16 -0400

    App now prints the user data to the console

--- server.js
+++ server.js
@@ -1,6 +1,37 @@
 //Importing libraries that we installed using npm
 const express = require("express")
 const app = express()
+const bcrypt = require("bcrypt") //Imported bcrypt package
+/**
+ * const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: ''
+
+})
+ */
+
+const users = []
+
+
+app.use(express.urlencoded({encoded: false}))
+
+app.post("/register", async(req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+        })
+        console.log(users); //Display newly registered users in the console
+        res.redirect("/login")
+    } catch (e) {
+        res.redirect("/register")
+    }
+})
 
 //Routes that the user can take
 //Homepage
@@ -17,5 +48,6 @@ app.get('/login', (req, res) => {
 app.get('/register', (req, res) => {
     res.render("register.ejs")
 })
+
 
 app.listen(3000)

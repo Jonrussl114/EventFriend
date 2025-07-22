875bc4a187e9879405940381f978940607a537a5
Jonrussl114 <jonrussl114@gmail.com>
2025-07-21 23:54:55 -0400

    Created the login and register pages layout (no real functionality yet except that they go to each other if their linked is clicked)

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+//Importing libraries that we installed using npm
 const express = require("express")
 const app = express()
 
+//Routes that the user can take
+//Homepage
 app.get('/', (req, res) => {
     res.render("index.ejs")
+})
+
+//Login Page
+app.get('/login', (req, res) => {
+    res.render("login.ejs")
+})
+
+//Register Page
+app.get('/register', (req, res) => {
+    res.render("register.ejs")
 })
 
 app.listen(3000)
